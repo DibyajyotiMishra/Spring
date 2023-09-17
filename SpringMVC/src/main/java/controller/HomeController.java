d3ab62d139e5cf65f554786e06cc5dffea4fc64e
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2023-09-18 00:30:42 +0530

    feat: learn sending data to View from Controller using Model object in  Spring MVC

--- SpringMVC/src/main/java/controller/HomeController.java
+++ SpringMVC/src/main/java/controller/HomeController.java
@@ -1,13 +1,15 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
         System.out.println("This is home page");
+        model.addAttribute("name", "Dibyajyoti M");
         return "index";
     }
 
--- SpringMVC/src/main/webapp/WEB-INF/views/index.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/views/index.jsp
@@ -4,5 +4,9 @@
 </head>
 <body>
 <h1>This is Home Page</h1>
+<%
+    String author = (String) request.getAttribute("name");
+%>
+<h2>Author: <%= author %></h2>
 </body>
 </html>
