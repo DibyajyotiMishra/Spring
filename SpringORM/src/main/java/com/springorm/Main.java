daba17e080d9a99388f8488235b6572e182b0e06
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2023-09-17 12:40:02 +0530

    feat: learn Spring and Hibernate ORM

--- SpringORM/src/main/java/com/springorm/Main.java
+++ SpringORM/src/main/java/com/springorm/Main.java
@@ -5,12 +5,71 @@ import com.springorm.entities.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int studentId;
+        String studentName;
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:config.xml");
         StudentDao studentDao = context.getBean("student", StudentDao.class);
-        Student student = new Student(5467, "Babul");
-        int result = studentDao.insert(student);
-        System.out.println("Row ID created/updated/deleted: " + result);
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+            System.out.println("Welcome to Student Manager");
+            System.out.println("Choose from one option: ");
+            System.out.println("1. Add a Student");
+            System.out.println("2. Delete a Student");
+            System.out.println("3. Get a Student's Data");
+            System.out.println("4. Get all Students' Data");
+            System.out.println("5. Update Student Data");
+            System.out.println("6. Exit");
+            System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Enter Student ID");
+                    studentId = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Enter Student name");
+                    studentName = scanner.nextLine();
+                    Student student = new Student(studentId, studentName);
+                    int rowId = studentDao.insert(student);
+                    System.out.println("Added Student with ID " + rowId);
+                    break;
+                case 2:
+                    System.out.println("Enter Student ID");
+                    studentId = scanner.nextInt();
+                    int result = studentDao.delete(studentId);
+                    System.out.println("No. of rows affected: " + result);
+                    break;
+                case 3:
+                    System.out.println("Enter Student ID");
+                    studentId = scanner.nextInt();
+                    System.out.println(studentDao.getStudent(studentId));
+                    break;
+                case 4:
+                    System.out.println(studentDao.getStudents());
+                    break;
+                case 5:
+                    System.out.println("Enter student ID");
+                    studentId = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Enter student Name");
+                    studentName = scanner.nextLine();
+                    Student newStudent = new Student(studentId, studentName);
+                    int res = studentDao.update(newStudent);
+                    System.out.println("No. of rows affected: " + res);
+                    break;
+                case 6:
+                    System.out.println("Exiting....");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Option. Try again...");
+                    break;
+            }
+        }
     }
 }
--- SpringORM/src/main/java/com/springorm/dao/StudentDao.java
+++ SpringORM/src/main/java/com/springorm/dao/StudentDao.java
@@ -4,6 +4,7 @@ import com.springorm.entities.Student;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public class StudentDao {
     private HibernateTemplate hibernateTemplate;
@@ -11,6 +12,30 @@ public class StudentDao {
     @Transactional
     public int insert(Student student) {
         return (Integer) this.hibernateTemplate.save(student);
+    }
+
+    public Student getStudent(int studentId) {
+        return (Student) this.hibernateTemplate.get(Student.class, studentId);
+    }
+
+    public List<Student> getStudents() {
+        return this.hibernateTemplate.loadAll(Student.class);
+    }
+
+    @Transactional
+    public int  delete(int studentId) {
+        Student student = this.hibernateTemplate.get(Student.class, studentId);
+        if (student != null) {
+            this.hibernateTemplate.delete(student);
+            return 1;
+        }
+        return 0;
+    }
+
+    @Transactional
+    public int update(Student student) {
+        this.hibernateTemplate.update(student);
+        return 1;
     }
 
     public HibernateTemplate getHibernateTemplate() {
