aed280419035bc900e85a457a5b3d6def77d3a57
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2023-09-18 00:07:45 +0530

    feat: learn Controller and Views config in  Spring MVC

---
+++ SpringMVC/src/main/java/controller/HomeController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String home() {
+        System.out.println("This is home page");
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        System.out.println("This is about page");
+        return "about";
+    }
+}
